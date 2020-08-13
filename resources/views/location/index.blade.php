35e6cd95ac047997867781d1848d4f76f3a6277f
lighteagle <sofyan.agus18@gmail.com>
2020-08-13 14:33:58 +0700

    Navbar + Layout

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -1,51 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@extends('layouts.master')
 
-    
-    
-<title>Location</title>
-</head>
-<body>
-    
-    <div class="container">
-
-        <h1>Location</h1>   
-        <div class="row">
-            <div class="col-12">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
-                Add Location
-                </button>
-            </div>
-            <!-- Alert Data -->
-            @if(session('sukses'))
+@section('content')
+    <h1>Location</h1>   
+    <div class="row">
+        <div class="col-12">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+            Add Location
+            </button>
+        </div>
+        <!-- Alert Data -->
+        @if(session('sukses'))
             <div class="alert alert-success" role="alert">
                 {{session('sukses')}}
             </div>
         @endif
-            <table class='table table-hover'>
-                <tr>
-                    <th>Code</th>
-                    <th>Name</th>
-                    <th>Action</th>
-                </tr>
-                
-                @foreach($data_location as $location)
-                <tr>
-                    <td>{{$location->code}}</td>
-                    <td>{{$location->name}}</td>
-                    <td><a href='/location/{{$location->id}}/edit' class='btn btn-warning btn-sm'>Edit </a>{{' | Delete'}}</td>
-                </tr>
-                @endforeach
-            </table>
-        </div>
+        <table class='table table-hover'>
+            <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+            
+            @foreach($data_location as $location)
+            <tr>
+                <td>{{$location->code}}</td>
+                <td>{{$location->name}}</td>
+                <td><a href='/location/{{$location->id}}/edit' class='btn btn-warning btn-sm'>Edit </a>{{' | Delete'}}</td>
+            </tr>
+            @endforeach
+        </table>
     </div>
-    
+
+        
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -76,12 +63,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</body>
-</html>
-
-
+@endsection
